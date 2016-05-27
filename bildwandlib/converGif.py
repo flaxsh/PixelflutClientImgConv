d0b65bc9f58ae6585a627e807d9292f2e9726e3f
theo <tbeffart@gmail.com>
2016-05-27 13:53:32 +0200

    rename files and add playGif proto

---
+++ bildwandlib/converGif.py
@@ -0,0 +1,16 @@
+#!/bin/python
+from bildwandlib import convert
+import sys
+from PIL import Image
+
+def main (path,offx=0, offy=0):
+    #TODO get frames from gif as lists of img objects
+    frames = []
+    #    frames = Image.open(path)
+    for img in frames:
+            print('\n'.join(convert.main(img,offx,offy)))
+
+if __name__ == '__main__' :
+		if len(sys.argv) == 4:
+			main(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]))
+		main(sys.argv[1])
---
+++ playGif.py
@@ -0,0 +1,10 @@
+#!/bin/python
+import bildwandlib.convertGif as imgc
+import sys
+def main(path, offx=0, offy=0):
+    print('\n'.join(imgc.main(path,offx,offy)))
+
+if __name__ == '__main__' :
+		if len(sys.argv) == 4:
+			main(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]))
+		main(sys.argv[1])
