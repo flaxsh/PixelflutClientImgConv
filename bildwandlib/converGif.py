d36fff5994ac7a4803b00fd856809fd8125fa410
theo <tbeffart@gmail.com>
2016-05-27 13:54:22 +0200

    print newline after each frame

--- bildwandlib/converGif.py
+++ bildwandlib/converGif.py
@@ -9,7 +9,7 @@ def main (path,offx=0, offy=0):
     #    frames = Image.open(path)
     for img in frames:
             print('\n'.join(convert.main(img,offx,offy)))
-
+            print('\n')
 if __name__ == '__main__' :
 		if len(sys.argv) == 4:
 			main(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]))
