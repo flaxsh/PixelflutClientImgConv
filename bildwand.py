60c4237c4dcfdcd9483333e1de9fe7026682e318
theo <tbeffart@gmail.com>
2016-05-27 02:27:14 +0200

    restructure

--- bildwand.py
+++ bildwand.py
@@ -8,23 +8,24 @@ def main (path,offx=0, offy=0):
 	img = Image.open(path)
 	im = np.array(img)
 	linelist = buildlines(im,len(im[1,1])==4,offx,offy)
-
+	print('\n'.join(linelist))
 
 #print(a)
 #print ('FOO',len(im),len(im[0]))
 
 def buildlines(im,a,offx,offy):
+	rlist = []
 	for y in range(len(im)):
 		for x in range(len(im[31])):
-			if a and im[y,x,3] == 0:
+			#skip fully transparent if alpha channel is present
+			if a and im[y,x,3] == 0 :
 				continue
-			rgb = (im[y,x,0],im[y,x,1],im[y,x,2])
-			rgbstr = ""
-			for i in range(3):
-				s = str(hex(rgb[i]))[2:]
-				rgbstr += s.zfill(2).upper()
-			print('PX', x+offx, y+offy,rgbstr)
-
+			rgbstr = buildrgbstr(im[y,x][:3])
+			rlist.append('PX %d %d %s' %(x+offx,y+offy,rgbstr) )
+	return rlist
+		
+def buildrgbstr(rgb):
+	return''.join(str(hex(c))[2:].zfill(2).upper() for c in rgb)
 
 if __name__ == '__main__' :
 		if len(sys.argv) == 4:
