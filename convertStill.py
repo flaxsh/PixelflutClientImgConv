10c42650eaaacad2bfdbe9632f201f0f63cddd4e
theo <tbeffart@gmail.com>
2016-05-28 18:10:50 +0200

    split functionality in still conv

--- convertStill.py
+++ convertStill.py
@@ -4,8 +4,10 @@ import sys
 from PIL import Image
 
 def main (path,offx=0, offy=0):
-    print('\n'.join(convert.main(Image.open(path).convert('RGBA'),offx,offy)))
+    print('\n'.join(generateFrame(path,offx,offy)))
 
+def generateFrame(path,offx,offy):
+    return convert.main(Image.open(path).convert('RGBA'),offx,offy)
 if __name__ == '__main__' :
 		if len(sys.argv) == 4:
 			main(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]))
